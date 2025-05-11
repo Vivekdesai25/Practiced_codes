7d5dd5ffd37cd711da3121ba8fede78065b93c52
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-11 23:11:02 +0530

    Update and rename complete_python_operators.py to Advance operators.py
    Operators

---
+++ Advance operators.py
@@ -0,0 +1,17 @@
+#ADVANCED OPERATORS
+
+#Identity operator 
+a_list = [10, 20]
+b_list = a_list
+same_ref = (a_list is b_list)
+new_list = [10, 20]
+not_same = (a_list is not new_list)
+
+# Membership operators
+colors = ['red', 'green', 'blue']
+has_red = 'red' in colors
+no_purple = 'purple' not in colors
+
+# Ternary operator
+marks = 75
+result = "Pass" if marks >= 35 else "Fail"
