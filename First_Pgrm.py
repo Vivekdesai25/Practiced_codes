839a022e3127c7cd4c04fd7bc261008607f36795
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-12 21:35:29 +0530

    Update and rename 1p.py to First_Pgrm.py

---
+++ First_Pgrm.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+print(' I m Vivek \\ Desai ')
+
+
+print("""My name is Vivek. 
+Now I'm just practicing Python programming, 
+so I'm using multiline comments.""")
+
+
+name = input("What's your name? ")
+print("Nice to meet you, " + name + "!")
