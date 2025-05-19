10210edc87cf29ee9a9b7685ba5fdc86ac1dbde7
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-19 20:26:00 +0530

    Create Perfect_no.java

---
+++ Perfect_no.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Pnumber{
+    int a;
+    public Pnumber(int a)
+    {
+        this.a=a;
+    }
+    public int cal()
+    {
+        int m=0;
+        for (int i=1;i<=a/2;i++)
+        {
+            if(a%i == 0)
+            {
+                m=m+i;
+            }
+        }
+        return m;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("enter number = ");
+        int v=sc.nextInt();
+        Pnumber od= new Pnumber(v);
+        int r=od.cal();
+        
+        if(r==v)
+        {
+            System.out.println("Its a perfect number");
+        }
+        else
+        {
+            System.out.println("Given no is not perfect number");
+        }
+       
+    }
+}
