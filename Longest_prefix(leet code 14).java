b330c01e5b837303a2e76bbd162ced887f86f75d
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-20 01:02:20 +0530

    Create Longest_prefix(leet code 14).java

---
+++ Longest_prefix(leet code 14).java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Long {
+    public static String Long(String str[])
+    {
+        Arrays.sort(str);
+        String fs=str[0];
+        String ss=str[str.length-1];
+        int i=0;
+        while (i<str.length)
+        {
+            if(fs.charAt(i)==ss.charAt(i))
+            {
+                i++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return i==0 ?" \"\" " : ss.substring(0,i);
+    }
+    public static void main(String[] args) {
+        String str[]={"oiar","pcarb","ctarot"};
+        System.out.println(Long(str));
+    }
+} 
