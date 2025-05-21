2a6a942f9948b373ac058bdeb7eba9cba19b9d90
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-21 18:26:50 +0530

    Create ASCII.java

---
+++ ASCII.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner (System.in);
+      System.out.println("Enter Any charector");
+     char ch = sc.next().charAt(0);
+     int ascii_value = (int)ch;
+     System.out.println("ASCII value of given charector is  "+ascii_value);
+     System.out.println("\n\n");
+     System.out.println("Enter Any number from 0 to 127");
+     int v=sc.nextInt();
+     char ASCII_no = (char)v;
+     System.out.println("ASCII charector of given number is "+ASCII_no);
+  }
+}
