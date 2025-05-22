46ea622c48ae23afa0da6ed001a988e07634a2fc
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-22 23:42:43 +0530

    Update GCD.py

--- GCD.py
+++ GCD.py
@@ -6,3 +6,19 @@ a=int(input ("en 1st no "))
 b=int(input("en 2nd no "))
 r=gcd(a,b)
 print("GCD is ",r)
+
+'''
+import java.util.*;
+
+public class Main {
+  public static int gcd(int a,int b){
+    if(b==0) return a;
+    else return gcd(b,a%b);
+  }
+    public static void main(String[] args) {
+      int r = gcd(10,15);
+      System.out.println("gcd is "+ " "+r);
+     
+  }
+}
+'''
