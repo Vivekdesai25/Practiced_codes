80c83f5c2df3150ccdf675cb002bb6aa4aa531f4
Vivekdesai25 <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-22 18:45:05 +0530

    Update Object as parameter.java

--- Object as parameter.java
+++ Object as parameter.java
@@ -18,7 +18,7 @@ public boolean cal(Check x){
  Check ob1= new Check(10,20);
  Check ob2= new Check(-2,-6);
  Check ob3= new Check(10,20);
-//Check ob3= new Check(ob1);
+//Check ob3= new Check(ob1); also same but different memory 
  System.out.println("ob1==ob2 "+ob1.cal(ob2));
  System.out.println("ob1==ob3 "+ob1.cal(ob3));
  }
