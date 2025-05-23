31a6b655b421647881488cb4a90aa53fe735c75b
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-23 20:39:06 +0530

    Update String_Palindrom_check.py

--- String_Palindrom_check.py
+++ String_Palindrom_check.py
@@ -1,1 +1,8 @@
+def received(s):
+    return s == s[::-1]
 
+  #s = ''.join(c.lower() for c in s if c.isalnum())  # clean input
+  #return s == ''.join(reversed(s))  # compare with reversed string
+
+
+print("Palindrome" if received("madam") else "Not a palindrome")
