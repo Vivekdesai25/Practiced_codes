3430943cc112da78b66e48d148b9dd300b702c24
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-23 18:10:13 +0530

    Create String_upr_lwr.java

---
+++ String_upr_lwr.java
@@ -0,0 +1,34 @@
+class UpperLowerCase {
+
+    public void display(String str, int p) {
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (p == 1 && Character.isUpperCase(ch)) {
+                System.out.println(ch);
+            } else if (p != 1 && Character.isLowerCase(ch)) {
+                System.out.println(ch);
+            }
+        }
+    }
+
+    public void display(String str,char chr){
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(chr=='v'){
+                char ch1 = Character.toLowerCase(ch);
+                if(ch1=='a' || ch1=='e' || ch1=='o' || ch1=='i' || ch1=='u'){
+                    System.out.println(ch);
+                }
+            }
+            else{
+                System.out.println(ch);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+      UpperLowerCase ob = new UpperLowerCase();
+      //ob.display("COMPuteR", 8);
+      ob.display("COMPUTER", 'v');
+    }
+}
