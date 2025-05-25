37306ca04baee7980ccac222182cce792b480b11
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-25 16:21:51 +0530

    Create LCM.py

---
+++ LCM.py
@@ -0,0 +1,11 @@
+import math
+n=int(input("en no1  "))
+n1=int(input("en no2 "))
+i=max(n,n1)
+while True:
+    if(i%n==0 and i%n1==0):
+        print ("LCM ",i)
+        break
+    i+=1
+LCM=(n*n1)/math.gcd(n,n1)
+print(LCM)
