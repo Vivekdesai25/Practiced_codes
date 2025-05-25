4d15cf6b2080ab11dc9ffefaac2591c45cc28f81
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-25 20:14:14 +0530

    Update Armstrong.py

--- Armstrong.py
+++ Armstrong.py
@@ -1,9 +1,18 @@
-n=int(input (" enter no ")
+n=int(input (" enter no "))
 original=n
 pow=len(str(n))
 res=0
 while n>0:
-  n=n%10
-  res=res+(n**pow)
+  m=n%10
+  res=res+(m**pow)
   n//=10
 print ("Armstrong" if res==original else "not Armstrong" )
+
+'''
+for i in range(1, 1000):
+    if function (i):
+        print(i)
+        
+armstrong no 153,370,371,9474
+ex 153 =1³+5³+3³
+'''
