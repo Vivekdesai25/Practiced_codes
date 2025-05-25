36ea9b3a9a1b27ea44027c7cf4295733a154917e
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-25 14:11:28 +0530

    Update Square root.py

--- Square root.py
+++ Square root.py
@@ -1,1 +1,11 @@
+import math 
+a=float(input("en value a "))
+b=float (input("en value b "))
+c=float(input("en value c "))
 
+k=(b**2 - 4*a*c)
+z=math.sqrt(k)
+
+r1=(-b+z)/(2*a)
+r2=(-b-z)/(2*a)
+print("The roots of the quadratic equation are:", r1, "and", r2)
