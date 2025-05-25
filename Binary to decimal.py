357961e0fcc704f1db32cb43e89180cde6697825
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-26 00:20:56 +0530

    Create Binary to decimal.py

---
+++ Binary to decimal.py
@@ -0,0 +1,12 @@
+n=input("Enter binary no ")
+
+def bi(l):
+    p=0
+    d=0
+    for i in reversed(l):
+        if i=='1':
+            d=d+2**p
+        p+=1
+    return d
+k=bi(n)
+print ("Decimal ",k)
