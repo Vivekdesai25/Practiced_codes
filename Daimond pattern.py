561cbd91f3800923cbe70fc6dd1eb653810f7e7b
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-05-26 16:08:48 +0530

    Create Daimond pattern.py

---
+++ Daimond pattern.py
@@ -0,0 +1,11 @@
+n=6
+for i in range (1,n+1):
+    print(" "*(n-i),end="")
+    for j in range (1,i+1):
+        print(chr(j+96),"",end="")
+    print()
+for i in range (n,0,-1):
+    print(" "*(n-i),end="")
+    for j in range (1,i+1):
+        print(chr(j+47),"",end="")
+    print()
