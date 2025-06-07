8c2b2c1ee4c4413bfbac59e70eacc55a8836582d
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-06-07 20:42:17 +0530

    Update Reverse string.py

--- Reverse string.py
+++ Reverse string.py
@@ -1,4 +1,4 @@
 n=input("Enter the String to reverse ")
 reversed_string = s[::-1]
-print (reversed_string )
-print ("".join(reversed(n)))
+print (reversed_string ) #output 1
+print ("".join(reversed(n))) #ourput 2
