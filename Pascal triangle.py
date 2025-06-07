cee02c4ad3f937a43fda9e922d149f11a46067a1
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-06-07 20:38:21 +0530

    Update Pascal triangle.py

--- Pascal triangle.py
+++ Pascal triangle.py
@@ -4,8 +4,9 @@ for i in range (0,n+1):
   k=1
   for j in range (0,i+1):
     print(k," ",end="")
-    k=k*(i-j)//(j+1) //important
+    k=k*(i-j)//(j+1)
     import math
+
 '''
 value = math.factorial(i) // (math.factorial(j) * math.factorial(i - j))  
 nCr or imort fact function
