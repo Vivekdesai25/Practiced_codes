fde0ce5a7817f57e61ef814685e5d76e4090951b
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-06-10 22:23:16 +0530

    Update Fib(without recursion).py

--- Fib(without recursion).py
+++ Fib(without recursion).py
@@ -1,7 +1,7 @@
-n = int(input("Enter the number of terms: "))
+8n = int(input("Enter the number of terms: "))
 a, b = 0, 1
 
 print("Fibonacci series:")
 for i in range(n):
     print(a, end=" ")
-    a, b = b, a + b #swap
+    a, b = b, a + b #swaping
