17586d4ff7866a69fe5d481b6c9b17bdef4a4d69
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-06-10 22:25:24 +0530

    Update string slice and reverse.py

--- string slice and reverse.py
+++ string slice and reverse.py
@@ -1,5 +1,4 @@
 s = "vivek desai"
-
 # reversed_each_word = " ".join(word[::-1] for word in s.split())
 # reversed_word_order = " ".join(s.split()[::-1])
 # fully_reversed = " ".join(word[::-1] for word in s.split()[::-1])
@@ -7,7 +6,6 @@ s = "vivek desai"
 reversed_each_word = " ".join("".join(reversed(word)) for word in s.split())
 reversed_word_order = "".join(" ".join(reversed(s.split())))
 fully_reversed = " ".join("".join(reversed(word)) for word in reversed(s.split()))
-
 
 print(reversed_each_word)    # Output: keviv iased
 print(reversed_word_order)   # Output: desai vivek
