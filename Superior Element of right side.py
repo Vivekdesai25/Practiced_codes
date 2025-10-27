38463c24070f246eaf7c1b7cec8ec0948053a9e5
Vivek Desai <153173803+Vivekdesai25@users.noreply.github.com>
2025-10-27 22:26:00 +0530

    Create Superior Element of right side.py

---
+++ Superior Element of right side.py
@@ -0,0 +1,15 @@
+n=int(input("enter no of elements\n"))
+a=[]
+
+element=input("en element\n").split(',')
+for i in range(n):
+  a.append(int(element[i]))
+# a=[1,8,5,0,6]
+sup=float('-inf')
+c=0
+for i in range(len(a)-1,-1,-1):
+  if a[i]>sup:
+    c+=1
+    sup=a[i]
+    
+print(c)
